c95b2ab3ea181d01e5d07b9ea5509a2dfb36c281
DaniAnikii <98807626+DaniAnikii@users.noreply.github.com>
2022-10-31 07:57:29 -0400

    Add files via upload

---
+++ problemas.py
@@ -0,0 +1,29 @@
+def getHitProbability(R: int, C: int, G: List[List[int]]) -> float:
+  a = 0.0
+  b = 0.0
+  for i in G:
+    for u in i:
+      if u == 0:
+          a += 1
+      if u == 1:
+          b += 1
+  return b/(a+b)
+
+
+def getSum(A: int, B: int, C: int) -> int:
+  # Write your code here
+  return A+B+C
+
+def getWrongAnswers(N: int, C: str) -> str:
+  strings = []
+  for i in (C):
+      strings.append(i)
+  for i in range(N):
+     if strings[i] == "A":
+          strings[i] = "B"
+     elif strings[i] == "B":
+           strings[i] = "A"
+  res = ""
+  for i in strings:
+      res = res + i
+  return res
